8399869ebb69fb455d48ca1381a4bba54607e3aa
Rick Strafy <rixafy@gmail.com>
2019-04-01 23:34:25 +0200

    Add RouteGroupFactory

---
+++ src/Route/Group/RouteGroupFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rixafy\Routing\Route\Group;
+
+class RouteGroupFactory
+{
+    public function create(RouteGroupData $routeGroupData): RouteGroup
+    {
+        return new RouteGroup($routeGroupData);
+    }
+}
