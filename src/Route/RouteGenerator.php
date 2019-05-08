5e8ef99b43f2bb49d2a052e96f9e5fe68ba689ec
Rixafy <rixafy@gmail.com>
2019-05-08 23:54:00 +0200

    Remove deprecated methods from RouteGenerator

--- src/Route/RouteGenerator.php
+++ src/Route/RouteGenerator.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Rixafy\Routing\Route;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Ramsey\Uuid\UuidInterface;
-use Rixafy\Routing\Route\Exception\RouteNotFoundException;
 
 class RouteGenerator
 {
@@ -27,58 +25,5 @@ class RouteGenerator
 		$this->routeRepository = $routeRepository;
 		$this->entityManager = $entityManager;
 		$this->routeFactory = $routeFactory;
-	}
-
-	/**
-	 * @deprecated
-	 */
-	public function generate(RouteData $data): Route
-	{
-		try {
-			$route = $this->update($data->target, $data->name);
-
-		} catch (RouteNotFoundException $e) {
-			$route = $this->create($data);
-		}
-
-		return $route;
-	}
-
-	/**
-	 * @deprecated
-	 */
-	public function create(RouteData $data): Route
-	{
-		$route = $this->routeFactory->create($data);
-		$this->entityManager->persist($route);
-
-		$siteNameCount = $this->routeRepository->getNameCounter($data->name, $data->site->getId());
-		$route->increaseSiteNameCounter($siteNameCount);
-
-		$groupNameCount = $this->routeRepository->getNameCounter($data->name, $data->site->getId(), $data->group->getId());
-		$route->increaseGroupNameCounter($groupNameCount);
-
-		return $route;
-	}
-
-	/**
-	 * @throws RouteNotFoundException
-	 * @deprecated
-	 */
-	public function update(UuidInterface $targetId, string $name): Route
-	{
-		$route = $this->routeRepository->getByTarget($targetId);
-
-		if ($name !== $route->getName()) {
-			$route->changeName($name);
-
-			$siteNameCount = $this->routeRepository->getNameCounter($name, $route->getSite()->getId());
-			$route->increaseSiteNameCounter($siteNameCount);
-
-			$groupNameCount = $this->routeRepository->getNameCounter($name, $route->getSite()->getId(), $route->getGroup()->getId());
-			$route->increaseGroupNameCounter($groupNameCount);
-		}
-
-		return $route;
 	}
 }
