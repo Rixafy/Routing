e3a929de4c0d42901cc123f94bf3a2669c226ed7
Rixafy <rixafy@gmail.com>
2019-06-17 22:54:14 +0200

    Fix code style

--- src/Route/RouteData.php
+++ src/Route/RouteData.php
@@ -11,30 +11,30 @@ use Rixafy\Routing\Route\Site\RouteSite;
 
 class RouteData
 {
-    /** @var string */
-    public $name;
+	/** @var string */
+	public $name;
 
-    /** @var string */
-    public $controller;
+	/** @var string */
+	public $controller;
 
-    /** @var string */
-    public $action = 'default';
+	/** @var string */
+	public $action = 'default';
 
-    /** @var string */
-    public $module = 'Front';
+	/** @var string */
+	public $module = 'Front';
 
-    /** @var UuidInterface */
-    public $target;
+	/** @var UuidInterface */
+	public $target;
 
-    /** @var array */
-    public $parameters = [];
+	/** @var array */
+	public $parameters = [];
 
-    /** @var Language */
-    public $language;
+	/** @var Language */
+	public $language;
 
-    /** @var RouteGroup */
-    public $group;
+	/** @var RouteGroup */
+	public $group;
 
-    /** @var RouteSite */
-    public $site;
+	/** @var RouteSite */
+	public $site;
 }
